af235ef83b45d3a6c240c9f636f776e3e9199681
Nico <nico35490@gmail.com>
2019-03-25 16:18:52 +0100

    Init TP3

--- TP3/app.js
+++ TP3/app.js
@@ -6,12 +6,12 @@ var app = express()
 const debug = require('debug')('tp3-server')
 const port = 3000
 
-debug('Booting')
 
+debug('Booting')
 app.get('/', function (req, res) {
 	debug(`Request received ${req.method} ${req.url}`)
 	res.status(200)
-	res.json("H")
+	res.json("ok");
 })
 
 // Listen on port, IP defaults to 127.0.0.1
