354fa980fcc0c5474e88649f5eac098c66a6140a
Nico <nico35490@gmail.com>
2019-03-25 11:51:33 +0100

    STEP 2 : Utilisation de stat (sync)

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const http = require('http')
 const fs = require('fs')
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    
-    res.end(JSON.stringify(fs.readdirSync("./")));
+    let dict = {};
+    let files = fs.readdirSync("./");
+    for (file in files){
+        dict[files[file]] = fs.statSync(files[file]);
+    }
+    res.end(JSON.stringify(dict));
 })
 server.listen(8000) // Port d'écoute
