e0270c057df65ddcad3eed011c2f372a92750d73
Nico <nico35490@gmail.com>
2019-03-25 11:31:38 +0100

    STEP 1 : Utilisation de fs (sync)

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Hello ESIR");
+const http = require('http') 
+const fs = require('fs')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    
+    res.end(JSON.stringify(fs.readdirSync("./")));
+})
+server.listen(8000) // Port d'écoute
