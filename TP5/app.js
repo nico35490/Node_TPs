cd14334fbdd22348f15a3f47caddf7756cb07e71
Nico <nico35490@gmail.com>
2019-04-08 13:54:34 +0200

    INIT TP5

--- TP5/app.js
+++ TP5/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
 
 const usersRouter = require('./routes/users-v1')
 const usersModel = require('./model/users')
@@ -7,9 +8,12 @@ const usersModel = require('./model/users')
 const app = express()
 
 app.use(bodyParser.json())
-app.use(require('helmet')())
-// On injecte le model dans le router. Ceci permet de supprimer la dépendance
-// directe entre le router et le modele
+
+// Activation de Helmet
+app.use(helmet({noSniff: true}))
+
+// On injecte le model dans les routers. Ceci permet de supprimer la dépendance
+// directe entre les routers et le modele
 app.use('/v1/users', usersRouter(usersModel))
 
 // For unit tests
